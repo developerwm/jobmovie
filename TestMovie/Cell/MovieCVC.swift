5653edd9d9d1f38723c37f6778e7f718da5a0ce6
taua <taua.one@gmail.com>
2018-10-28 22:04:29 -0300

    first commit

---
+++ TestMovie/Cell/MovieCVC.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCVC.swift
+//  TestMovie
+//
+//  Created by Tiago on 28/10/2018.
+//  Copyright © 2018 Tauã. All rights reserved.
+//
+
+import UIKit
+
+class MovieCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TestMovie/Config/Api.swift
@@ -0,0 +1,9 @@
+//
+//  Api.swift
+//  TestMovie
+//
+//  Created by Tiago on 28/10/2018.
+//  Copyright © 2018 Tauã. All rights reserved.
+//
+
+import Foundation
